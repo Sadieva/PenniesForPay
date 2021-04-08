49882053707c91e579ab5f72ebafd99307ca54a8
Ojosan <koshka.slytherin@gmail.com>
2021-04-07 20:06:00 -0400

    simple loop

---
+++ PenniesForPay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PenniesForPay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int numberPennies = 1; 
+            int days = 1;
+
+            
+            while (userInput>=days) 
+            {
+                
+                userInput = userInput * 2;
+                
+                
+                Console.WriteLine("ammount of pennies earned for day "+days+": "+numberPennies);
+                days++;
+
+            }
+        }
+    }
+}
